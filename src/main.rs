41a570921a153ea80500b4c4260034678330b850
Arve Seljebu <arve.seljebu@gmail.com>
2019-07-08 01:08:58 +0200

    https://stackoverflow.com/questions/54609316/how-do-i-prevent-a-timeout-problem-while-streaming-large-files-with-an-actix-web/56926683#56926683

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,24 @@
-use actix_web::error::{ErrorBadRequest, ErrorInternalServerError};
-use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
-use futures::future;
+use actix_web::client::Client;
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
 use futures::future::Future;
 
 fn main() {
-    HttpServer::new(|| App::new().route("/", web::to_async(handler)))
-        .bind("127.0.0.1:8000")
-        .expect("Cannot bind to port 8000")
-        .run()
-        .expect("Unable to run server");
+    HttpServer::new(|| {
+        App::new()
+            .data(Client::new())
+            .route("/", web::to_async(handler))
+    })
+    .bind("127.0.0.1:8000")
+    .expect("Cannot bind to port 8000")
+    .run()
+    .expect("Unable to run server");
 }
 
-fn handler(req: HttpRequest) -> impl Future<Item = HttpResponse, Error = Error> {
-    has_client_header(&req)
-        .and_then(|client| {
-            operation_that_returns_future(client)
-                .map_err(|_| ErrorInternalServerError("operation failed"))
+fn handler(client: web::Data<Client>) -> impl Future<Item = HttpResponse, Error = Error> {
+    client.get("https://gensho.ftp.acc.umu.se/debian-cd/current/amd64/iso-cd/debian-10.0.0-amd64-netinst.iso")
+        .send()
+        .map_err(Error::from)
+        .and_then(|res| {
+            HttpResponse::build(res.status()).streaming(res)
         })
-        .map(|result| HttpResponse::Ok().body(result))
-}
-
-fn has_client_header(req: &HttpRequest) -> impl Future<Item = String, Error = Error> {
-    if let Some(Ok(client)) = req.headers().get("x-client-id").map(|h| h.to_str()) {
-        future::ok(client.to_owned())
-    } else {
-        future::failed(ErrorBadRequest("invalid x-client-id header"))
-    }
-}
-
-fn operation_that_returns_future(client: String) -> impl Future<Item = String, Error = ()> {
-    future::ok(client)
 }
