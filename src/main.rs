566de0f8ef1a62b6d99a8266220418a3953b118f
Arve Seljebu <arve.seljebu@gmail.com>
2019-07-07 23:14:15 +0200

    https://stackoverflow.com/questions/55691151/how-to-return-error-to-caller-from-handler-with-client/56926129#56926129

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::client::{Client, SendRequestError};
+use actix_web::error::{ErrorBadGateway, ErrorInternalServerError};
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use futures::future::Future;
 
 fn main() {
-    HttpServer::new(|| App::new().route("/", web::to(handler)))
+    HttpServer::new(|| App::new().data(Client::new()).route("/", web::to(handler)))
         .bind("127.0.0.1:8000")
         .expect("Cannot bind to port 8000")
         .run()
         .expect("Unable to run server");
 }
 
-fn handler(req: HttpRequest) -> impl Responder {
-    if let Some(content_type) = get_content_type(&req) {
-        format!("Got content-type = '{}'", content_type)
-    } else {
-        "No content-type header.".to_owned()
-    }
-}
-
-fn get_content_type<'a>(req: &'a HttpRequest) -> Option<&'a str> {
-    req.headers().get("content-type")?.to_str().ok()
+fn handler(client: web::Data<Client>) -> Box<Future<Item = HttpResponse, Error = Error>> {
+    Box::new(
+        client
+            .get("https://httpbin.org/get")
+            .no_decompress()
+            .send()
+            .map_err(|err| match err {
+                SendRequestError::Connect(error) => {
+                    ErrorBadGateway(format!("Unable to connect to httpbin: {}", error))
+                }
+                error => ErrorInternalServerError(error),
+            })
+            .and_then(|response| {
+                let mut result = HttpResponse::build(response.status());
+                let headers = response
+                    .headers()
+                    .iter()
+                    .filter(|(h, _)| *h != "connection" && *h != "content-length");
+                for (header_name, header_value) in headers {
+                    result.header(header_name.clone(), header_value.clone());
+                }
+                Ok(result.streaming(response))
+            }),
+    )
 }
