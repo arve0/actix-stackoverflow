7befa40aefed148c712531969abfe87cba2b235f
Arve Seljebu <arve.seljebu@gmail.com>
2019-09-25 22:26:06 +0200

    https://stackoverflow.com/questions/57892819/how-to-return-an-early-response-from-an-actix-web-middleware/58070229#58070229

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
-use actix_web::web::{Data, ServiceConfig};
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Payload;
+use actix_web::error::ErrorUnauthorized;
+use actix_web::{web, App, Error, FromRequest, HttpRequest, HttpResponse, HttpServer};
 
 fn main() {
-    let db = String::from("simplified example");
-
-    HttpServer::new(move || App::new().configure(config_app(db.clone())))
+    HttpServer::new(move || App::new().route("/", web::to(index)))
         .bind("127.0.0.1:3000")
         .expect("Can not bind to '127.0.0.1:3000'")
         .run()
         .unwrap();
 }
 
-fn config_app(db: String) -> Box<Fn(&mut ServiceConfig)> {
-    Box::new(move |cfg: &mut ServiceConfig| {
-        cfg.data(db.clone())
-            .service(web::resource("/notes").route(web::get().to(notes)));
-    })
+fn index(_: Authorized) -> HttpResponse {
+    HttpResponse::Ok().body("authorized")
 }
 
-fn notes(db: Data<String>) -> HttpResponse {
-    HttpResponse::Ok().body(["notes from ", &db].concat())
+struct Authorized;
+
+impl FromRequest for Authorized {
+    type Error = Error;
+    type Future = Result<Self, Error>;
+    type Config = ();
+
+    fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
+        if is_authorized(req) {
+            Ok(Authorized)
+        } else {
+            Err(ErrorUnauthorized("not authorized"))
+        }
+    }
+}
+
+fn is_authorized(req: &HttpRequest) -> bool {
+    if let Some(value) = req.headers().get("authorized") {
+        // actual implementation that checks header here
+        dbg!(value);
+        true
+    } else {
+        false
+    }
 }
